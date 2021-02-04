b9b31edb7eae3300e25703b66df12c62417dad68
alialaba <alialaba079@gmail.com>
2021-02-04 09:00:12 -0800

    added sass and made styling with it

--- script/index.js
+++ script/index.js
@@ -5,3 +5,16 @@ not they can afford an item based on their available funds. 💸
 2. Style it as you wish 💅 
 
 */
+let btn = document.querySelector("button");
+let costPrice = document.getElementById('itemPrice');
+let fundSaved = document.getElementById("fund");
+let displayOutput = document.getElementById("output");
+btn.addEventListener("click", () => {
+    let funding = parseFloat(fundSaved.value);
+    let cp = parseFloat(costPrice.value);
+    if (funding > 0 && cp > 0) {
+        funding >= cp ? displayOutput.textContent = "You can get some item" :
+            displayOutput.textContent = "Keep funding you will be fine 💸"
+
+    }
+})
